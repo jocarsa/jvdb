50a23a671bcec82198c8191df99409dcae6a97c5
jocarsa <info@josevicentecarratala.com>
2023-10-17 17:46:34 +0200

    Especificar base de datos

--- jvdb.c
+++ jvdb.c
@@ -3,10 +3,19 @@
 
 int main(int argc,char *argv[]){
     FILE *archivo;
-    char *nombrearchivo = argv[1];
-    archivo = fopen(strcat(nombrearchivo,".txt"),"a");
-    char *texto = argv[2];
+    char *basededatos = argv[1];
+    char *nombrearchivo = argv[2];
+    
+    char ruta[100];
+    strcpy(ruta,basededatos);
+    strcat(ruta,"-");
+    strcat(ruta,nombrearchivo);
+    strcat(ruta,".txt");
+    
+    archivo = fopen(ruta,"a");
+    char *texto = argv[3];
     fputs(strcat(texto,"\n"),archivo);
     fclose(archivo);
+    
     return 0;
 }
