4eab95b95b0e75213e6dca582abe88e9a05e8b4f
jocarsa <info@josevicentecarratala.com>
2023-10-17 17:58:37 +0200

    Seleccionar e insertar

--- jvdb.c
+++ jvdb.c
@@ -4,18 +4,23 @@
 int main(int argc,char *argv[]){
     FILE *archivo;
     char *operacion = argv[1];
+    char *basededatos = argv[2];
+    char *nombrearchivo = argv[3];
+    char ruta[100];
+    strcpy(ruta,basededatos);
+    strcat(ruta,"-");
+    strcat(ruta,nombrearchivo);
+    strcat(ruta,".txt");
+    
     if(strcmp(operacion,"select") == 0){
-        printf("te doy datos");
+        archivo = fopen(ruta,"r");
+        printf("te doy datos:\n");
+        char linea[1024];
+        while(fgets(linea,sizeof(linea),archivo) != NULL){
+            printf("Linea: %s",linea);
+        }
+        
     }else if(strcmp(operacion,"insert") == 0){
-        char *basededatos = argv[2];;
-        char *nombrearchivo = argv[3];
-
-        char ruta[100];
-        strcpy(ruta,basededatos);
-        strcat(ruta,"-");
-        strcat(ruta,nombrearchivo);
-        strcat(ruta,".txt");
-
         archivo = fopen(ruta,"a");
         char *texto = argv[4];
         fputs(strcat(texto,"\n"),archivo);
