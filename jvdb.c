2a8d0eb1e7e0180f4b0962526f542e281063704e
jocarsa <info@josevicentecarratala.com>
2023-10-17 17:50:22 +0200

    atrapar operaciones con if else if

--- jvdb.c
+++ jvdb.c
@@ -3,19 +3,27 @@
 
 int main(int argc,char *argv[]){
     FILE *archivo;
-    char *basededatos = argv[1];
-    char *nombrearchivo = argv[2];
+    char *operacion = argv[1];
+    if(strcmp(operacion,"select") == 0){
+        printf("te doy datos");
+    }else if(strcmp(operacion,"insert") == 0){
+        char *basededatos = argv[2];;
+        char *nombrearchivo = argv[3];
+
+        char ruta[100];
+        strcpy(ruta,basededatos);
+        strcat(ruta,"-");
+        strcat(ruta,nombrearchivo);
+        strcat(ruta,".txt");
+
+        archivo = fopen(ruta,"a");
+        char *texto = argv[4];
+        fputs(strcat(texto,"\n"),archivo);
+        fclose(archivo);
+    }else{
+        printf("operación no válida");
+    }
     
-    char ruta[100];
-    strcpy(ruta,basededatos);
-    strcat(ruta,"-");
-    strcat(ruta,nombrearchivo);
-    strcat(ruta,".txt");
-    
-    archivo = fopen(ruta,"a");
-    char *texto = argv[3];
-    fputs(strcat(texto,"\n"),archivo);
-    fclose(archivo);
     
     return 0;
 }
