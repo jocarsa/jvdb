513e74da1d6de14ffbd9ef05272d4965b2e801b1
jocarsa <info@josevicentecarratala.com>
2023-10-17 17:32:21 +0200

    Entrada y salida

--- jvdb.c
+++ jvdb.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(){
+int main(int argc,char *argv[]){
     FILE *archivo;
-    archivo = fopen("basededatos.txt","w");
-    char *texto = "JVDB";
-    fputs(texto,archivo);
+    archivo = fopen("basededatos.txt","a");
+    char *texto = argv[1];
+    fputs(strcat(texto,"\n"),archivo);
     fclose(archivo);
     return 0;
 }
