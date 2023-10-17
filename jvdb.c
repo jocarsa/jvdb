df3601c44de09c56f4093d12b9f649cf809842c7
jocarsa <info@josevicentecarratala.com>
2023-10-17 17:26:14 +0200

    Guardar datos en archivo

--- jvdb.c
+++ jvdb.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 int main(){
-    printf("JVDB");
+    FILE *archivo;
+    archivo = fopen("basededatos.txt","w");
+    char *texto = "JVDB";
+    fputs(texto,archivo);
+    fclose(archivo);
     return 0;
 }
