9b913f55d69f859b1c8007f2fdbfb1be2e009212
jocarsa <info@josevicentecarratala.com>
2023-10-26 22:37:45 +0200

    Crear colecciones

--- jvdb.c
+++ jvdb.c
@@ -6,18 +6,17 @@ int main(int argc,char *argv[]){
     char *operacion = argv[1];
     char *basededatos = argv[2];
     char *coleccion = argv[3];
-    char *documento = argv[4];
-    char ruta[100];
-    strcpy(ruta,basededatos);
-    strcat(ruta,"/");
-    strcat(ruta,coleccion);
-    strcat(ruta,"/");
-    strcat(ruta,documento);
-    strcat(ruta,".json");
     
-    printf("la ruta es: %s",ruta);
-
     if(strcmp(operacion,"select") == 0){
+        char *documento = argv[4];
+        char ruta[100];
+        strcpy(ruta,"db/");
+        strcat(ruta,basededatos);
+        strcat(ruta,"/");
+        strcat(ruta,coleccion);
+        strcat(ruta,"/");
+        strcat(ruta,documento);
+        strcat(ruta,".json");
         archivo = fopen(ruta,"r");
         printf("te doy datos:\n");
         char linea[1024];
@@ -26,10 +25,31 @@ int main(int argc,char *argv[]){
         }
         
     }else if(strcmp(operacion,"insert") == 0){
+        char *documento = argv[4];
+        char ruta[100];
+        strcpy(ruta,"db/");
+        strcat(ruta,basededatos);
+        strcat(ruta,"/");
+        strcat(ruta,coleccion);
+        strcat(ruta,"/");
+        strcat(ruta,documento);
+        strcat(ruta,".json");
         archivo = fopen(ruta,"w");
-        char *texto = argv[4];
+        char *texto = argv[5];
         fputs(strcat(texto,"\n"),archivo);
         fclose(archivo);
+    }else if(strcmp(operacion,"create_collection") == 0){
+        char rutacoleccion[100];
+        strcpy(rutacoleccion,"db/");
+        strcat(rutacoleccion,basededatos);
+        strcat(rutacoleccion,"/");
+        strcat(rutacoleccion,coleccion);
+        printf(rutacoleccion);
+        if(mkdir(rutacoleccion,0777) == 0){
+            printf("ok");
+        }else{
+            printf("ko");
+        }
     }else{
         printf("operación no válida");
     }
