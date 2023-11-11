07b8e2862e76c8fe054ed4d79d9dd8c8bcd245ce
jocarsa <info@josevicentecarratala.com>
2023-11-11 10:28:27 +0100

    Objeto

--- conexion.py
+++ conexion.py
@@ -1,6 +1,6 @@
 import subprocess
 
-class jvdb:
+class Jvdb:
     def __init__(self,basededatos):
         self.basededatos = basededatos
     def insert(self,coleccion,documento,contenido):
@@ -8,10 +8,11 @@ class jvdb:
         self.coleccion = coleccion
         self.documento = documento
         self.contenido = contenido
-            comando = '"C:\\Users\\Jose Vicente\\Documents\\GitHub2\\jvdb\\jvdb.exe" '+operacion+' '+basededatos+' '+coleccion+' '+documento+' "'+contenido+'"'
-            resultado = subprocess.run(comando,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,text=True)
-            if resultado.returncode == 0:
-                return("ok")
-            else:
-                return("ko")
+        comando = '"C:\\Users\\Jose Vicente\\Documents\\GitHub2\\jvdb\\jvdb.exe" '+operacion+' '+basededatos+' '+coleccion+' '+documento+' "'+contenido+'"'
+        resultado = subprocess.run(comando,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,text=True)
+        if resultado.returncode == 0:
+            return("ok")
+        else:
+            return("ko")
 
+Conexion1 = Jvdb("miempresa")
