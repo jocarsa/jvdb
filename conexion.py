28d157a784a3f4a786a74a7b33f3c5d7c30b3806
jocarsa <info@josevicentecarratala.com>
2023-11-11 10:22:28 +0100

    Parametrizacion

--- conexion.py
+++ conexion.py
@@ -1,6 +1,12 @@
 import subprocess
 
-comando = '"C:\\Users\\Jose Vicente\\Documents\\GitHub2\\jvdb\\jvdb.exe" insert miempresa clientes cliente6 "esto es una prueba"'
+operacion = "insert"
+basededatos = "miempresa"
+coleccion = "clientes"
+documento = "cliente7"
+contenido = "este es otro contenido de prueba"
+
+comando = '"C:\\Users\\Jose Vicente\\Documents\\GitHub2\\jvdb\\jvdb.exe" '+operacion+' '+basededatos+' '+coleccion+' '+documento+' "'+contenido+'"'
 resultado = subprocess.run(comando,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,text=True)
 
 if resultado.returncode == 0:
