3cbf928f20fd7e58b15409b0aee3e2b7953944ab
jocarsa <info@josevicentecarratala.com>
2023-11-11 10:31:05 +0100

    Clase funcionando

--- conexion.py
+++ conexion.py
@@ -8,7 +8,7 @@ class Jvdb:
         self.coleccion = coleccion
         self.documento = documento
         self.contenido = contenido
-        comando = '"C:\\Users\\Jose Vicente\\Documents\\GitHub2\\jvdb\\jvdb.exe" '+operacion+' '+basededatos+' '+coleccion+' '+documento+' "'+contenido+'"'
+        comando = '"C:\\Users\\Jose Vicente\\Documents\\GitHub2\\jvdb\\jvdb.exe" '+self.operacion+' '+self.basededatos+' '+self.coleccion+' '+self.documento+' "'+self.contenido+'"'
         resultado = subprocess.run(comando,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,text=True)
         if resultado.returncode == 0:
             return("ok")
@@ -16,3 +16,4 @@ class Jvdb:
             return("ko")
 
 Conexion1 = Jvdb("miempresa")
+Conexion1.insert("clientes","cliente9","este es otro contenido de prueba")
