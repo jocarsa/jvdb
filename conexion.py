54af9a39a6cc15199a4fff06ae40949b02ffa66a
jocarsa <info@josevicentecarratala.com>
2023-11-11 10:27:03 +0100

    Definicion de la clase

--- conexion.py
+++ conexion.py
@@ -1,15 +1,17 @@
 import subprocess
 
-operacion = "insert"
-basededatos = "miempresa"
-coleccion = "clientes"
-documento = "cliente7"
-contenido = "este es otro contenido de prueba"
+class jvdb:
+    def __init__(self,basededatos):
+        self.basededatos = basededatos
+    def insert(self,coleccion,documento,contenido):
+        self.operacion = "insert"
+        self.coleccion = coleccion
+        self.documento = documento
+        self.contenido = contenido
+            comando = '"C:\\Users\\Jose Vicente\\Documents\\GitHub2\\jvdb\\jvdb.exe" '+operacion+' '+basededatos+' '+coleccion+' '+documento+' "'+contenido+'"'
+            resultado = subprocess.run(comando,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,text=True)
+            if resultado.returncode == 0:
+                return("ok")
+            else:
+                return("ko")
 
-comando = '"C:\\Users\\Jose Vicente\\Documents\\GitHub2\\jvdb\\jvdb.exe" '+operacion+' '+basededatos+' '+coleccion+' '+documento+' "'+contenido+'"'
-resultado = subprocess.run(comando,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,text=True)
-
-if resultado.returncode == 0:
-    print("ok")
-else:
-    print("ko")
